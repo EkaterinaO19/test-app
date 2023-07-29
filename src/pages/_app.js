006fecf9781d4a3d1c191348c25c5ffb1a031ed6
EkaterinaO19 <ekshepa@gmail.com>
2023-07-29 17:24:15 +0300

    hot fix

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,31 +3,35 @@ import Header from "@/pages/components/header/Header";
 import Footer from "@/pages/components/footer/Footer";
 import Wrapper from "@/pages/components/wrapper/Wrapper";
 import styles from '@/styles/figures.module.scss'
-import { Inter } from 'next/font/google'
+import {Inter} from 'next/font/google'
 import Image from "next/image";
 import purple_light from "@/assets/images/purple_light.svg";
 import purple_ball from "@/assets/images/purple_ball.png";
 import red_ball from "@/assets/images/red_ball.png";
 import red_ball_mini from "@/assets/images/red_ball_mini.png";
 import red_light from "@/assets/images/red_light.svg";
+import useLayoutWidth from "@/hooks/useLayoutWidth";
 
 
-const inter = Inter({ subsets: ['cyrillic-ext'] })
+const inter = Inter({subsets: ['cyrillic-ext']})
 
-export default function App({ Component, pageProps }) {
-  return (
-      <div className={inter.className}>
-          <Wrapper>
-              {/*<Image src={purple_light} alt={'purple_light'} className={styles.purple_light}/>*/}
-              {/*<Image src={purple_ball} alt={'purple_ball'} className={styles.purple_ball}/>*/}
-              {/*<Image src={red_ball} alt={'red_ball'} className={styles.red_ball}/>*/}
-              {/*<Image src={red_ball_mini} alt={'red_ball_mini'} className={styles.red_ball_mini}/>*/}
-              {/*<Image src={red_light} alt={'red_light'} className={styles.red_light}/>*/}
-              <Header />
-              <Component {...pageProps} />
-              <Footer />
-          </Wrapper>
-      </div>
+export default function App({Component, pageProps}) {
+    const is1440Width = useLayoutWidth(1440);
 
-      )
+    return (<div className={inter.className}>
+            <Wrapper>
+                {is1440Width && (<>
+                        <Image src={purple_light} alt={'purple_light'} className={styles.purple_light}/>
+                        <Image src={purple_ball} alt={'purple_ball'} className={styles.purple_ball}/>
+                        <Image src={red_ball} alt={'red_ball'} className={styles.red_ball}/>
+                        <Image src={red_ball_mini} alt={'red_ball_mini'} className={styles.red_ball_mini}/>
+                        <Image src={red_light} alt={'red_light'} className={styles.red_light}/>
+                    </>)}
+                <Header/>
+                <Component {...pageProps} />
+                <Footer/>
+            </Wrapper>
+        </div>
+
+    )
 }
